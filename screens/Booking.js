275760526a2e611f9c04d481c1c8effdf82320a0
tylerasa <sarpongasaresylvester@gmail.com>
2021-12-06 00:05:38 +0000

    layout for calender and swiper set

--- screens/Booking.js
+++ screens/Booking.js
@@ -1,29 +1,27 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import Calendar from 'react-native-ep-calendar'
-
-// let markedDates = [
-//     {
-//       date: new Date(Date.now()),
-//       markerType: 'myMarker'
-//     },
-//   ];
-
-//   let markerTypes = [
-//     {
-//       markerType: 'myMarker',
-//       markerTextStyle: {color: 'red'}.
-//       markerTouchableStyle = {backgroundColor: 'transparent'},
-//       markerColumnStyle = null
-//     },
-//   ];
-
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Calendar, CalendarList, Agenda } from "react-native-calendars";
+import SwipeButton from 'rn-swipe-button';
 export default function Booking() {
-    return (
-        <View>
-               
-        </View>
-    )
+  return (
+    <View>
+      <Calendar
+        markedDates={{
+          "2012-05-16": { selected: true, marked: true, selectedColor: "blue" },
+          "2012-05-17": { marked: true },
+          "2012-05-18": { marked: true, dotColor: "red", activeOpacity: 0 },
+          "2012-05-19": { disabled: true, disableTouchEvent: true },
+        }}
+      />
+       <SwipeButton
+            // enableReverseSwipe
+            // onSwipeSuccess={() => updateSwipeStatusMessage('Slide success!')}
+            railBackgroundColor="#a493d6"
+            thumbIconBackgroundColor="#FFFFFF"
+            title="Slide to unlock"
+          />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
